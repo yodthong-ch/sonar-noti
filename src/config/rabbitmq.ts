98cae51d5ece45d1de895d5666e1e255881f99a7
P. Tanakhan <prachatiwat@dek-d.com>
2021-04-21 10:24:52 +0700

    fix: replace env

--- src/config/rabbitmq.ts
+++ src/config/rabbitmq.ts
@@ -7,8 +7,8 @@ const config: amqplib.Options.Connect = {
     hostname: process.env.DEKD_RABBITMQ_HOST || 'localhost',
     port: parseInt(process.env.DEKD_RABBITMQ_PORT || '5672'),
     vhost: process.env.APP_RABBITMQ_VHOST || '/default',
-    username: process.env.APP_RABBITMQ_USERNAME,
-    password: process.env.APP_RABBITMQ_PASSWORD,
+    username: process.env.APP_RABBITMQ_USERNAME_CORE,
+    password: process.env.APP_RABBITMQ_PASSWORD_CORE,
 }
 
 const defaultOptions:{[x: string]: amqplib.Options.AssertExchange} = {
